ec1d48287d5c9ea6ea306cb0be198b7bbe946401
Mohammad Daoud <mohammad.daoud@shahid.net>
2023-12-04 09:10:57 +0300

    feat: Added DispatchGroup to group network requests

--- Bankey/AccountSummaryViewController.swift
+++ Bankey/AccountSummaryViewController.swift
@@ -28,20 +28,15 @@ class AccountSummaryViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        setupNavigationBar()
-    }
-    
-    func setupNavigationBar(){
-        navigationItem.rightBarButtonItem = logoutBarButtonItem
     }
 }
 
 extension AccountSummaryViewController {
     func setup(){
+        setupNavigationBar()
         setupTableView()
         setupTableViewHeader()
-        //fetchAccounts()
-        fetchDataAndLoadViews()
+        fetchData()
     }
     
     private func setupTableView(){
@@ -69,6 +64,9 @@ extension AccountSummaryViewController {
         tableView.tableHeaderView = headerView
     }
     
+    func setupNavigationBar(){
+        navigationItem.rightBarButtonItem = logoutBarButtonItem
+    }
 }
 
 extension AccountSummaryViewController : UITableViewDataSource {
@@ -100,28 +98,35 @@ extension AccountSummaryViewController {
 
 // MARK: - Networking
 extension AccountSummaryViewController {
-    private func fetchDataAndLoadViews() {
+    private func fetchData() {
+        let group = DispatchGroup()
         
+        group.enter()
         fetchProfile(forUserId: "1") { result in
             switch result {
             case .success(let profile):
                 self.profile = profile
                 self.configureTableHeaderView(with: profile)
-                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
         }
         
+        group.enter()
         fetchAccounts(forUserId: "1") { result in
             switch result {
             case .success(let accounts):
                 self.accounts = accounts
                 self.configureTableCells(with: accounts)
-                self.tableView.reloadData()
             case .failure(let error):
                 print("fetch accounts error \(error.localizedDescription)")
             }
+            group.leave()
+        }
+        
+        group.notify(queue: .main){
+            self.tableView.reloadData()
         }
     }
     
